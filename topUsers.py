5aa2ff1281a5eb7f0544f1db4de6dd8b13246d4a
Pedro Seixas <pedrodfs15@gmail.com>
2022-07-04 12:56:57 -0300

    Fixed DB Verification

--- topUsers.py
+++ topUsers.py
@@ -4,9 +4,9 @@ from mysql.connector import Error
 def sort_users():
         try:
             connection = mysql.connector.connect(host='remotemysql.com', database='lQcUi31XZz', user='lQcUi31XZz', password='8nIEHO3Rx3')
-            update_top_users_table = "SELECT autor, count(autor) as repeticoes FROM top_users GROUP BY autor ORDER BY repeticoes DESC LIMIT 5"
+            update_top_users = "SELECT autor, count(autor) as repeticoes FROM memes_recebidos GROUP BY autor ORDER BY repeticoes DESC LIMIT 5"
             cursor = connection.cursor()
-            cursor.execute(update_top_users_table)
+            cursor.execute(update_top_users)
             linhas = cursor.fetchall()
  
         except Error as erro:
